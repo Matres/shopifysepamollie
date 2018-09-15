35b6fb0c861f28888e6a9d171786d0089e7ea4aa
Matthijs Alderliefste <matthijs.alderliefste@gmail.com>
2018-09-15 14:54:04 +0200

    Initial Repository Creation

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+ * Shopify SEPA Mollie Recurring Payments
+ * Description: Enable SEPA payments recurring using Mollie for Dutch merchants
+ * 
+ * Matthijs Alderliefste
+ * 15 - 9 - 2018
+
+
+// authenticate with Shopify 
+
+//
